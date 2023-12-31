7264d26bbf8f18414663121f128492957a5666bb
Zombix3000 <133593034+Zombix3000@users.noreply.github.com>
2023-12-31 15:15:12 +0100

    Add files via upload

--- myHome/src/main/java/me/zombix/myhome/MyHome.java
+++ myHome/src/main/java/me/zombix/myhome/MyHome.java
@@ -4,7 +4,11 @@ import me.zombix.myhome.commands.HomeCommand;
 import me.zombix.myhome.commands.SetHomeCommand;
 import me.zombix.myhome.Config.ConfigManager;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.plugin.java.JavaPlugin;
+import me.zombix.myhome.commands.ReloadCommand;
+import me.zombix.myhome.commands.MyHomeCommand;
+import me.zombix.myhome.Config.CommandsTabCompleter;
 
 public class MyHome extends JavaPlugin {
 
@@ -15,7 +19,7 @@ public class MyHome extends JavaPlugin {
         configManager = new ConfigManager(this);
         configManager.setupConfig();
 
-        getLogger().info("myHome has been enabled!");
+        getLogger().info("Plugin myHome has been enabled!");
 
         registerCommands();
     }
@@ -23,15 +27,21 @@ public class MyHome extends JavaPlugin {
     @Override
     public void onDisable() {
         configManager.saveHomesConfig();
-        getLogger().info("myHome has been disabled!");
+        getLogger().info("Plugin myHome has been disabled!");
     }
 
     private void registerCommands() {
         CommandExecutor setHomeCommand = new SetHomeCommand(configManager);
         CommandExecutor homeCommand = new HomeCommand(this, configManager);
+        CommandExecutor reloadCommand = new ReloadCommand(configManager);
+        CommandExecutor myHomeCommand = new MyHomeCommand(this, configManager);
+        TabCompleter commandsTabCompleter = new CommandsTabCompleter();
 
         getCommand("sethome").setExecutor(setHomeCommand);
         getCommand("home").setExecutor(homeCommand);
+        getCommand("reload").setExecutor(reloadCommand);
+        getCommand("myhome").setExecutor(myHomeCommand);
+        getCommand("myhome").setTabCompleter(commandsTabCompleter);
     }
 
 }
